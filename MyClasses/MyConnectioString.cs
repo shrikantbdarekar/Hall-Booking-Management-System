b53faa2a95e867f848f848a4289518a78bb84150
Shrikant Darekar <shrikantbdarekar@hotmail.com>
2025-03-13 23:18:52 +0530

    my connection updated

--- MyClasses/MyConnectioString.cs
+++ MyClasses/MyConnectioString.cs
@@ -14,11 +14,13 @@ namespace HallBookingManagementSystem.MyClasses
             {
                 // Without user id and password
                 // return Properties.Settings.Default.ConnestionStringShort;
-                // return "Server=.\\sqlexpress19;Database=HallBookingDb;Integrated Security=True;";
+                //return "Server=.\\sqlexpress19;Database=HallBookingDb;Integrated Security=True;";
+                return "Server=.\\sqlexpress22;Database=HallBookingDb;Integrated Security=True;";
 
                 // With user id and password
                 // return Properties.Settings.Default.ConnestionStringFull;
-                return "Server=.\\sqlexpress19;Database=HallBookingDb;User Id=sa;Password=sqlexpress@19;";
+                //return "Server=.\\sqlexpress19;Database=HallBookingDb;User Id=sa;Password=pass@1234;";
+                //return "Server=.\\sqlexpress22;Database=HallBookingDb;User Id=sa;Password=pass@1234;";
             }
         }
     }
