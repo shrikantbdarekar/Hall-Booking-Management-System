87ddc85b45e1940afd68c8430d59186424d30256
Shrikant Darekar <shrikantbdarekar@hotmail.com>
2025-04-19 16:04:40 +0530

    screenshots added

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace HallBookingManagementSystem.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -37,7 +37,7 @@ namespace HallBookingManagementSystem.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Mr. Mark Zuckerberg\r\nMr. Sundar Pichai\r\nMr. Bill Gates")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Mr. Zuckerberg\r\nMr. Pichai\r\nMr. Gates")]
         public string DesignedDvelopedBy {
             get {
                 return ((string)(this["DesignedDvelopedBy"]));
